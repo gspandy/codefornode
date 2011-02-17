81e61bb97c97716e98ef1da10b02d8c6e6dd3d6b
aaron <aaron.pritzlaff@bjss.com>
2011-02-17 13:53:20 +0000

    nodes are normalized, cardinality needs sorting

--- src/main/scala/com/porpoise/codefornode/CodeFromNode.scala
+++ src/main/scala/com/porpoise/codefornode/CodeFromNode.scala
@@ -9,7 +9,7 @@ object Cardinality extends Enumeration {
 
 import Cardinality._
 
-case class Field(name : String, fieldType : Type, cardinality : Cardinality = OneToOne, defaultValue: Option[String] = None) {
+case class Field(name : String, fieldType : Type, cardinality : Cardinality = OneToOne) {
   def attribute(index : Int) = fieldType.fields(index)
   def apply(index : Int) = fieldType.fields(index)
   def cardString = if (cardinality == OneToOne) "1" else "*"
@@ -18,32 +18,13 @@ case class Field(name : String, fieldType : Type, cardinality : Cardinality = On
 
 case class Type(name : String, simpleFields : Set[String] = Set.empty, fields : Seq[Field] = Nil) {
   def types = fields.map(f => f.fieldType)
-  def allSubtypes : Iterable[Type] = types ++ fields.flatMap(f => f.fieldType.allSubtypes)
+  def allSubtypes : Seq[Type] = types ++ fields.flatMap(f => f.fieldType.allSubtypes)
   def allSubtypeNames = allSubtypes.map(_.name)
   def complexFieldNames = fields.map(_.toString)
   override def toString = "%s [%s]".format(name, (simpleFields ++ complexFieldNames).mkString(",")) 
 }
 
 object Type {
-
-   val testXml = <books>
-	   <book name="Oliver Twist">
-		   <chapters>
-			   <chapter name="Introduction">
-				   <page title="page one">some content</page>
-				   <page id="an id">some more content</page>
-			   </chapter>
-			   <chapter name="Conclusion">
-				   <page footnote="some footnote" />
-			   </chapter>
-		   </chapters>
-	   </book>
-	   <book name="B2">
-			<chapter name="Some chapter outside a chapters element">
-				<page color="red">some content</page>
-			</chapter>
-	   </book>
-   </books>
 
   private def flattenFields(allFields: Seq[Field]) : Seq[Field] = {
       // convert all these children into a map of field names to fields  
@@ -73,7 +54,7 @@ object Type {
          new Field(name, merged, cardinality=c)
       }
    }
-         
+
    implicit def apply(xml : Node) : Type = newType(xml)(apply _)
 
    /**
@@ -82,13 +63,34 @@ object Type {
     * the fields to point to the new merged types 
     */
    def normalize(root : Type) = {
-     val typeByName = root.allSubtypes.groupBy(t => t.name)
-     val (singleFields, multiFields) = typeByName.partition{ case (name, types) => types.size == 1 }
+     def update(root: Type)(f : Type => Type) : Type = {
+       val newFields = root.fields.map{old =>
+         val newType = update( f(old.fieldType) )(f)
+         new Field(old.name, newType, old.cardinality)
+       }
+       new Type(root.name, root.simpleFields, newFields)
+     }
      
-     // merge any duplicate types
-     val flattenedTypes = multiFields.mapValues{ types => flattenTypes(types.toSeq) }
-     val allTypes = singleFields ++ flattenedTypes
+     def unifyTypes : Map[String, Type] = {
+		 // group all types by their names
+	     val typeByName = root.allSubtypes.groupBy(t => t.name)
+	     
+	     // partition the list into singles (elements which have only appeared once) and 
+	     // elements which have appeared multiple times 
+	     var (singleFields, multiFields) = typeByName.partition{ case (name, types) => types.size == 1 }
+	     
+	     // now merge the duplicate elements (elements with the same names)
+	     val flattenedTypes = multiFields.mapValues{ types => flattenTypes(types.toSeq) }
+	
+	     // finally put the maps back together -- the original "singles" map and our newly flattened (merged) map
+	     val single : Map[String, Type] = singleFields.mapValues{ case head :: Nil => head }     
+	     single ++ flattenedTypes
+     }
      
+     val allTypes = unifyTypes
+     
+     // update all fields with the unified types
+     update(root){ oldType => allTypes(oldType.name) }
    }
       
    implicit def newType(xml : Node)( typeFactory : Node => Type) : Type = {
---
+++ src/main/scala/com/porpoise/codefornode/TestXml.scala
@@ -0,0 +1,27 @@
+package com.porpoise.codefornode
+
+import scala.xml._
+
+object TestXml {
+
+   def n = Type.normalize(Type(xml))
+
+   def xml = <books>
+	   <book name="Oliver Twist">
+		   <chapters>
+			   <chapter name="Introduction">
+				   <page title="page one">some content</page>
+				   <page id="an id">some more content</page>
+			   </chapter>
+			   <chapter name="Conclusion">
+				   <page footnote="some footnote" />
+			   </chapter>
+		   </chapters>
+	   </book>
+	   <book name="B2">
+			<chapter name="Some chapter outside a chapters element">
+				<page color="red">some content</page>
+			</chapter>
+	   </book>
+   </books>
+}
