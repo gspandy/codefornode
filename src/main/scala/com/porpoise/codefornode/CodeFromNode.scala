8316dbd6a5a8eeb851a3f0cf42a2f0bf940301e6
aaron <aaron.pritzlaff@bjss.com>
2011-02-17 11:58:13 +0000

    merging fields

--- src/main/scala/com/porpoise/codefornode/CodeFromNode.scala
+++ src/main/scala/com/porpoise/codefornode/CodeFromNode.scala
@@ -2,16 +2,63 @@ package com.porpoise.codefornode
 
 import scala.xml._
 
-case class Field(name : String, fieldType : Type, defaultValue: Option[String] = None)
+object Cardinality extends Enumeration {
+  type Cardinality = Value
+  val OneToOne, OneToMany = Value
+} 
 
-class Type(name : String, simpleFields : Seq[String] = Nil, fields : Seq[Field] = Nil)
+import Cardinality._
+
+case class Field(name : String, fieldType : Type, cardinality : Cardinality = OneToOne, defaultValue: Option[String] = None)
+
+case class Type(name : String, simpleFields : Seq[String] = Nil, fields : Seq[Field] = Nil)
 
 object Type {
 
-   def apply(xml : Node) : Type = {
-      val attFields = attributes(xml).keySet.toSeq
-      val fields = xml.nonEmptyChildren.map( child => new Field(name(child), apply(child))).toList
-      new Type(name(xml), attFields, fields)
+   val xml = <books>
+   <book name="Oliver Twist">
+   <chapters>
+   <chapter name="Introduction">
+   <page title="page one">some content</page>
+   <page id="an id">some more content</page>
+   </chapter>
+   </chapters>
+   </book>
+   </books>
+
+  /**
+   * A type may appear once as <book name='asdf' /> and elsewhere as <book id='123' />.
+   * Here we merge the properties for all elements with similar names
+   */
+  def flatten(types : Seq[Type]) : Type =  {
+    types.head
+  }
+  
+   private def merge(fieldByName : Map[String, Seq[Field]], c : Cardinality) = {
+      for ((name, fields) <- fieldByName ) yield {
+         val merged = flatten(fields.map(_.fieldType))
+         new Field(name, merged, cardinality=c)
+      }
+   }
+  
+   implicit def apply(xml : Node) : Type = {
+      val attFields = (attributes(xml).keySet + "text").toSeq
+      val children = xml.nonEmptyChildren.filter { 
+        case e : Elem => true
+        case other => false
+      }
+      // create a new field for *every* child (we will have duplicates!)
+      val allFields = children.map( child => new Field(name(child), apply(child)))
+      
+      // convert all these children into a map of field names to fields  
+      val fieldsByName = allFields.groupBy(f => f.name)
+      val (singleFields, multiFields) = fieldsByName.partition{ case (name, fields) => fields.size == 1 }
+      
+      val flattenedSingleFields = merge(singleFields, OneToOne)
+      val flattenedListFields = merge(multiFields, OneToMany)
+      
+      val fields = flattenedSingleFields ++ flattenedListFields  
+      new Type(name(xml), attFields, fields.toSeq)
    }
 
   def name(xml : NodeSeq) = {
