05978c3d900a266e08ab6dc7bfad0b0b17ce09a6
Aaron Pritzlaff <aaron@porpoiseltd.com>
2012-04-18 20:36:34 +0100

    lanugage change added

--- src/main/scala/com/porpoise/codefornode/Main.scala
+++ src/main/scala/com/porpoise/codefornode/Main.scala
@@ -1,23 +1,30 @@
 package com.porpoise.codefornode
 
-import com.porpoise.codefornode.ui._
-import com.porpoise.gen.beans._
-import scala.xml._
-import scala.collection._
 import java.io.File
-import com.google.common.io.Files
+
+import scala.xml.XML
+
 import com.google.common.base.Charsets
+import com.google.common.io.Files
+import com.porpoise.codefornode.ui.CodeForNodePanel
+import com.porpoise.codefornode.ui.TargetLanguage
+import com.porpoise.codefornode.ui.TargetLanguage._
+import com.porpoise.gen.beans.BeanGenerator
 
 object Main {
 
   def main(args: Array[String]) = {
     object Controller extends CodeForNodePanel.Controller {
-      override def generate(xmlFile: File, destDir: File, pckg: String) = {
+      override def generate(xmlFile: File, destDir: File, pckg: String, lang: TargetLanguage) = {
         val inputXml = XML.loadFile(xmlFile)
         val root = CodeForNode(inputXml)
         val model = XmlToModel.typeToModel(pckg, root)
         Files.write(model.toXml(), new File(destDir, "model.xml"), Charsets.UTF_8)
-        BeanGenerator.generateMain(model, destDir)
+        lang match {
+          case Java => BeanGenerator.generateMain(model, destDir)
+          case Scala => ScalaGenerator.generateMain(model, destDir)
+        }
+
         "success"
       }
     }
