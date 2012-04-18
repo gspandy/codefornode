0a72e4d771946970b6296591a9a972d4ca0b326e
Aaron Pritzlaff <aaron@porpoiseltd.com>
2012-04-18 18:06:42 +0100

    updated to sbt 10

--- src/main/scala/com/porpoise/codefornode/Main.scala
+++ src/main/scala/com/porpoise/codefornode/Main.scala
@@ -10,24 +10,24 @@ import com.google.common.base.Charsets
 
 object Main {
 
-  def main(args : Array[String]) = {
+  def main(args: Array[String]) = {
     object Controller extends CodeForNodePanel.Controller {
-      override def generate(xmlFile : File , destDir : File ,pckg : String ) = {
+      override def generate(xmlFile: File, destDir: File, pckg: String) = {
         val inputXml = XML.loadFile(xmlFile)
         val root = CodeForNode(inputXml)
         val model = XmlToModel.typeToModel(pckg, root)
         Files.write(model.toXml(), new File(destDir, "model.xml"), Charsets.UTF_8)
-        BeanGenerator.generate(model, destDir)
+        BeanGenerator.generateMain(model, destDir)
         "success"
       }
     }
 
-    if(args.length == 1)
-        XPaths.apply(XML.loadFile(args(0)))
-    else if(args.length > 1)
-        println("Usage: Main [xml file]%n\tEither open the code-for-node UI (with no args) or, if given the location of an xml file,%n\t print its xpaths to standard out".format())
+    if (args.length == 1)
+      XPaths.apply(XML.loadFile(args(0)))
+    else if (args.length > 1)
+      println("Usage: Main [xml file]%n\tEither open the code-for-node UI (with no args) or, if given the location of an xml file,%n\t print its xpaths to standard out".format())
 
-    if(args.length == 0)
+    if (args.length == 0)
       CodeForNodePanel.showCodeForNode(Controller)
   }
 }
